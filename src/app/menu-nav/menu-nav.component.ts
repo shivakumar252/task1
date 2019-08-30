35484b5b4a9fdd6d67afeefb597d6fad438fcfe6
User1 <user1@Skycliffitdomain.com>
2019-08-30 14:31:47 +0530

    first commit

---
+++ src/app/menu-nav/menu-nav.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatMenuTrigger } from '@angular/material';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-menu-nav',
+  templateUrl: './menu-nav.component.html',
+  styleUrls: ['./menu-nav.component.css']
+})
+export class MenuNavComponent implements OnInit {
+ closeResult: string;
+  selectedOption:string;
+  constructor(private modalService: NgbModal) { }
+
+  ngOnInit() {}
+
+  openEdit(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+
+}
+
+
+
+
